e00c673a0952e53e883446be34c04cfd8d9cddff
Julio Cesar Santos <juliotecinfo00@gmail.com>
2026-09-23 15:12:14 -0300

    fix: Resolvendo pull request e passando o teste de identação do codigo

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function aplicarDesconto(total, percentual) {
 }
 
 console.log("testando a acuracia da IA")
-console.log(testando a acuracia da IA)
+console.log("Resolvendo o pull request e passando o teste da IA")
 
 module.exports = { calcularTotal, aplicarDesconto };
